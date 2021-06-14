f7f1d69527c1435e9963dc60d6eb65acaf6b9661
Subhrodip Mohanta <subhrodipmohanta@gmail.com>
2021-06-14 23:54:59 +0530

    Initial Commit

---
+++ src/main/java/xyz/subho/clone/twitter/TwitterBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.subho.clone.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/subho/clone/twitter/TwitterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.subho.clone.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

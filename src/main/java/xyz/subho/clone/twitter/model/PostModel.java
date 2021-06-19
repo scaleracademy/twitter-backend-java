7b2dff59b1cccbb7ef06f532f0182a3b84d2057f
jeesou <soumyabanerjee0807@gmail.com>
2021-06-20 01:48:32 +0530

    formatting changes

--- src/main/java/xyz/subho/clone/twitter/model/PostModel.java
+++ src/main/java/xyz/subho/clone/twitter/model/PostModel.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import lombok.Data;
-import xyz.subho.clone.twitter.entity.Like;
 import xyz.subho.clone.twitter.entity.User;
 
 @Data

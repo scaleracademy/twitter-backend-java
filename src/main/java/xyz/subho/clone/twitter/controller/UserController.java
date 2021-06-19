ebcad39d697f7fb6721415e4b8f8b25a696521b6
jeesou <48854046+jeesou@users.noreply.github.com>
2021-06-19 23:14:45 +0530

    Addition of User controller and service (#7)

--- src/main/java/xyz/subho/clone/twitter/controller/UserController.java
+++ src/main/java/xyz/subho/clone/twitter/controller/UserController.java
@@ -18,12 +18,19 @@
 
 package xyz.subho.clone.twitter.controller;
 
+import java.util.List;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xyz.subho.clone.twitter.model.UserResponse;
@@ -46,18 +53,49 @@ public class UserController {
 
     UserResponse userResponse;
 
-    if (utility.isUUID(userNameOrUserId)) {
-      var uuid = utility.converStringToUUID(userNameOrUserId);
-      userResponse = new UserResponse(userService.getUserByUserId(uuid));
-      return new ResponseEntity<>(userResponse, HttpStatus.OK);
-    } else {
-      var username =
-          userNameOrUserId.startsWith("@") ? userNameOrUserId.substring(1) : userNameOrUserId;
+    if (userNameOrUserId.startsWith("@")) {
+      var username = userNameOrUserId.substring(1);
       userResponse = new UserResponse(userService.getUserByUserName(username));
       return new ResponseEntity<>(userResponse, HttpStatus.OK);
     }
+    var userId = utility.converStringToUUID(userNameOrUserId);
+    userResponse = new UserResponse(userService.getUserByUserId(userId));
+    return new ResponseEntity<>(userResponse, HttpStatus.OK);
   }
 
-  // ResponseEntity<Boolean> makeNewUser	()
+  @PostMapping
+  ResponseEntity<UserResponse> createUser(@RequestBody UserResponse userResponse) {
+    UserResponse createdUser = new UserResponse(userService.addUser(userResponse));
+    return new ResponseEntity<>(createdUser, HttpStatus.OK);
+  }
 
+  @PatchMapping
+  ResponseEntity<UserResponse> updateUser(@RequestBody UserResponse userResponse) {
+    UserResponse updatedUser = new UserResponse(userService.editUser(userResponse));
+    return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+  }
+
+  @PutMapping("/{userId}/follow")
+  ResponseEntity<HttpStatus> addFollower(@PathVariable UUID userId) {
+    userService.addFollower(userId);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @DeleteMapping("/{userId}/follow")
+  ResponseEntity<HttpStatus> removeFollower(@PathVariable("userId") UUID userId) {
+    userService.removeFollower(userId);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @GetMapping("/{userId}/followers")
+  ResponseEntity<List<UserResponse>> getFollowers(@PathVariable("userId") UUID userId) {
+    List<UserResponse> followers = userService.getFollowers(userId);
+    return new ResponseEntity<>(followers, HttpStatus.OK);
+  }
+
+  @GetMapping("/{userId}/followings")
+  ResponseEntity<List<UserResponse>> getFollowings(@PathVariable("userId") UUID userId) {
+    List<UserResponse> followings = userService.getFollowings(userId);
+    return new ResponseEntity<>(followings, HttpStatus.OK);
+  }
 }
